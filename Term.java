475450c9182e1f2cdac32de56b75808de01976f4
matthewhilde <matthewhilde8@gmail.com>
2018-10-19 12:01:51 -0400

    Update Term.java

--- Term.java
+++ Term.java
@@ -5,7 +5,7 @@ import java.lang.String;
 
 /*
  * CPS 350: Assignment 3
- * 10/9/18
+ * 10/19/18
  * @author Matthew Hildebrandt
  * @author Antall Nguyen
  * 
@@ -16,45 +16,43 @@ public class Term implements Comparable <Term>{
 
 	private long weight;
 	private String query;
-
+	
+	/* Initializes a term with the given query string and weight. */
 	public Term(String query, long weight) {	
-		if(query.equals(null)) throw new NullPointerException(); 
-		if(weight < 0) throw new IllegalArgumentException();
-		
+		// Throws exception if the query is null
+		if(query.equals(null)) {
+			throw new java.lang.NullPointerException(); 
+		}
+		// Throw exception if weight of the word is less than 0
+		if(weight < 0) {
+			throw new java.lang.IllegalArgumentException();
+		}
 		this.query = query;
 		this.weight = weight;
 	}
 
-	public static Comparator<Term> byReverseWeightOrder(){
+	/* Compares the two terms in descending order by weight. */
+	public static Comparator<Term> byReverseWeightOrder() {
 		return new ReverseWeightOrder();
 	}
 
-	private static class ReverseWeightOrder implements Comparator<Term>{
-		@Override
-		public int compare(Term a, Term b) {
-			// TODO Auto-generated method stub
-			return (int)(a.weight - b.weight); // return + if a > b, - if a < b, 0 if a = b 
-		}
-	}
 	
-	public static Comparator<Term> byPrefixOrder(int r){
+	public static Comparator<Term> byPrefixOrder(int r) {
 		if(r < 0) throw new IllegalArgumentException();
 		
-		return new byPrefixOrder(r);
+		return new PrefixOrder(r);
 	}
 	
-	private static class byPrefixOrder implements Comparator<Term>{
+	private static class PrefixOrder implements Comparator<Term>{
 
-		private int r;
+		private final int r;
 
-		public byPrefixOrder(int r) {
-			// TODO Auto-generated constructor stub
+		private PrefixOrder(int r) {
 			this.r = r;
 		}
 
 		@Override
 		public int compare(Term a, Term b) {
-			// TODO Auto-generated method stub
 			int lengthTerm1 = a.query.length();
 			int lengthTerm2 = b.query.length();
 			String word1, word2;
@@ -64,15 +62,21 @@ public class Term implements Comparable <Term>{
 				word1 = a.query.toLowerCase().substring(0, r);
 				word2 = b.query.toLowerCase().substring(0, r);
 				
-				return word1.compareTo(word2);
+				int cmp = word1.compareTo(word2);
+				if      (cmp <= -1) return -1;
+                else if (cmp >= 1)  return 1;
+                else                return 0;
 			}
 			
 			//if one term length is bigger than r
-			else if(lengthTerm1 >= r&& lengthTerm2 < r) {
+			else if(lengthTerm1 >= r && lengthTerm2 < r) {
 				word1 = a.query.toLowerCase().substring(0,r);
 				word2 = b.query.toLowerCase();
 				
-				return word1.compareTo(word2);		
+				int cmp = word1.compareTo(word2);
+				if      (cmp <= -1) return -1;
+                else if (cmp >= 1)  return 1;
+                else                return 0;
 			}
 			
 			//if the other term length is bigger than r
@@ -80,25 +84,45 @@ public class Term implements Comparable <Term>{
 				word1 = a.query.toLowerCase();
 				word2 = b.query.toLowerCase().substring(0, r);
 				
-				return word1.compareTo(word2);
+				int cmp = word1.compareTo(word2);
+				if      (cmp <= -1) return -1;
+                else if (cmp >= 1)  return 1;
+                else                return 0;
 			}
 			//if both terms's lengths are less than r
 			else {
 				word1 = a.query.toLowerCase();
 				word2 = b.query.toLowerCase();
-
-				return word1.compareTo(word2);
-			}			
+				
+				int cmp = word1.compareTo(word2);
+				if      (cmp <= -1) return -1;
+                else if (cmp >= 1)  return 1;
+                else                return 0;
+			}
 		}
 	}
 
-		public String toString() {		
-			return weight + "/t" + query;
-		}
+	public String toString() {		
+		return weight + " \t" + query;
+	}
 
 	@Override
 	public int compareTo(Term that) {
-		// TODO Auto-generated method stub
 		return this.query.compareTo(that.query);
 	}
+	
+	private static class ReverseWeightOrder implements Comparator<Term> {
+		@Override
+		public int compare(Term t1, Term t2) {
+			Long w1 = t1.weight;
+			Long w2 = t2.weight;
+			
+			int cmp = w1.compareTo(w2);
+			if      (cmp <= -1) return 1;  // Opposite result of forward order
+            else if (cmp >= 1)  return -1; // Opposite result of forward order
+            else                return 0;
+			
+		}
+	}
+	
 }
